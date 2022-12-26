160682d83afa7f5fad93884f76843b2d5456c7e4
sriram403 <78860461+sriram403@users.noreply.github.com>
2022-12-26 20:24:47 +0530

    asdf

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, request, flash
-
+import os
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
 
-@app.route("/hello")
+app.config["UPLOAD_FOLDER"] = os.path.join("static","images")
+pic = os.path.join(app.config["UPLOAD_FOLDER"],"angel.jpeg")
+@app.route("/")
 def index():
 	flash("what's your name?")
-	return render_template("index.html")
+	return render_template("index.html",pic1=pic)
 
 @app.route("/greet", methods=['POST', 'GET'])
 def greeter():
 	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
-	return render_template("index.html")
+	return render_template("index.html",pic1=pic)
+
+if __name__=="__main__":
+	app.run(debug=True)
